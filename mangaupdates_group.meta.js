857a6c74eb33b15234a23354deeeaab17ebfb63e
loadletter <loadletter@users.noreply.github.com>
2014-01-13 14:22:40 +0100

    Release 1.4.8

--- mangaupdates_group.meta.js
+++ mangaupdates_group.meta.js
@@ -2,7 +2,7 @@
 // @name        Mangaupdates Groups Fix
 // @namespace   Mangaupdates Groups Fix (https://github.com/loadletter/mangaupdates-urlfix)
 // @include     http://www.mangaupdates.com/groups.html?id=*
-// @version     1.4.7
+// @version     1.4.8
 // @downloadURL https://github.com/loadletter/mangaupdates-urlfix/raw/master/mangaupdates_group.user.js
 // @updateURL   https://github.com/loadletter/mangaupdates-urlfix/raw/master/mangaupdates_group.meta.js
 // @grant       none
