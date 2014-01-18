0d6bac385782c8d4a07d18d2bb40b7c55a467dec
loadletter <loadletter@users.noreply.github.com>
2014-01-18 19:11:38 +0100

    Added functions to manage the json files

--- update.py
+++ update.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 
-import os, re, sys, urllib2, multiprocessing
+import os, re, sys, urllib2, multiprocessing, json
 
+def jsonloadf(filename):
+	f = open(filename)
+	return json.load(f, 'utf-8')
 
+def jsonsavef(filename, data):
+	f = open(filename, 'w')
+	json.dump(data, f, sort_keys=True, indent=4, separators=(',', ': '), encoding='utf-8')
 
 if __name__ == '__main__':
     main()
