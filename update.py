26cdf5106e1cb9f56c2a4f52c60f129436797a71
loadletter <loadletter@users.noreply.github.com>
2014-01-18 22:29:05 +0100

    Added functions to get the data from the database

--- update.py
+++ update.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+import os, re, sys, urllib2, json, psycopg2
+from dbconf import DSN
 
-import os, re, sys, urllib2, multiprocessing, json
+WWWBROWSER = "firefox"
 
 def jsonloadf(filename):
 	f = open(filename)
@@ -9,6 +11,45 @@ def jsonloadf(filename):
 def jsonsavef(filename, data):
 	f = open(filename, 'w')
 	json.dump(data, f, sort_keys=True, indent=4, separators=(',', ': '), encoding='utf-8')
+	
+def dumpqueue()
+	conn = psycopg2.connect(DSN)
+	cur = conn.cursor()
+	
+	data = None
+	try:
+		cur.execute("SELECT id, groupid, groupwww, refer, remoteip, uagent FROM posts")
+		data = cur.fetchall()
+	except:
+		print "Database Error"
+		conn.rollback()
+		
+	if not data:
+		print "No data, exiting..."
+		sys.exit(0)
+	
+	conn.commit()
+	cur.close()
+	conn.close()
+	
+	return data
 
+def deletequeued(id_list):
+	conn = psycopg2.connect(DSN)
+	cur = conn.cursor()
+	
+	delete_list = map(lambda x: (x,), id_list)
+	try:
+		cur.executemany("DELETE FROM posts WHERE id = %s", delete_list)
+	except:
+		print "Database Error, couldn't delete:", repr(a)
+		conn.rollback()
+	
+	conn.commit()
+	cur.close()
+	conn.close()
+	
+	
+	
 if __name__ == '__main__':
     main()
