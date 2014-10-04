a531e95b32bae3ba2f6a9f51513929c5d254e2bf
loadletter <loadletter@users.noreply.github.com>
2014-10-04 19:45:03 +0200

    Added current groups

--- src/groups/example.js
+++
@@ -1,7 +0,0 @@
-var urlfix_grouplist_shard = 2;
-var urlfix_grouplist = {
-    "2" : "should ids be kept as strings",
-    "22" : "or moved to integers",
-    "42" : "integers would provide real sorting",
-    "62" : "strings are alredy used"
-}
