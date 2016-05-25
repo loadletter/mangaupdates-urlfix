499aa28b07211eb2c5f8a89ca25a943b807cb3b3
loadletter <loadletter@users.noreply.github.com>
2016-05-26 00:16:16 +0200

    Revision 134
    Updated 3040
    Added 7808

--- src/groups/8.js
+++ src/groups/8.js
@@ -240,6 +240,7 @@ var urlfix_grouplist = {
     "7728": "http://mouthsealed.tumblr.com/",
     "7748": "http://maria-ms.livejournal.com/",
     "7768": "http://vicecaptainsugawara.tumblr.com/",
+    "7808": "http://xxicedtea.tumblr.com/",
     "808": "http://www.tenshi-tachi.com",
     "828": "http://cienna.livejournal.com/",
     "88": "http://www.mnm-scanlations.com",
